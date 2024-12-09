64e1c56cf93d02e912e9212b1e0b6e843c96d63c
oagengmotlapele <peacecontact18@gmail.com>
2024-12-09 23:48:59 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Password-Strength-Checker-Pro",
-    version="0.1.0",
+    name="password_strength_checker_pro",
+    version="1.0.0.1",
     packages=find_packages(),
     install_requires=[
         # Add your external dependencies here, for example:
